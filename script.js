57575a02b4af9b712a23e80dcaf056999721dc20
Ratul Biswas <98582203+Ratul167@users.noreply.github.com>
2022-10-28 10:38:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 // Code credits goes to Ratul167
 
 
-  async function getapi("Get"){
-    const url ="https://zenquotes.io/api/random";
-    const response = await fetch(url);
+
+  async function getapi(){
+
+    let manyurl=['https://some-random-api.ml/facts/bird','https://some-random-api.ml/facts/bird','https://some-random-api.ml/facts/dog','https://some-random-api.ml/facts/fox','https://some-random-api.ml/facts/panda']
+
+
+
+
+
+    var opurl= manyurl[Math.floor(Math.random() * manyurl.length)];
+    console.log(opurl)
+    const response = await fetch(opurl);
     let  data = await response.json();
-    var quoteText =data[0]["q"]
-    var authorName =data[0]["a"]
+    var quoteText =data["fact"]
+    // var authorName =data[0]["author"]
     console.log(quoteText)
     document.getElementById("op").innerHTML = quoteText;
-    document.getElementById("name").innerHTML = authorName;
+    // document.getElementById("name").innerHTML = authorName;
     
   }
 
