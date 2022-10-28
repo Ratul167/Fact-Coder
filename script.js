82fb9a1b0379b0bd4860ad44a655a13a8e570dba
Ratul Biswas <98582203+Ratul167@users.noreply.github.com>
2022-10-28 11:49:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,16 @@
     
   }
 
+(async () => {
+    let manyurl=['https://some-random-api.ml/facts/bird','https://some-random-api.ml/facts/bird','https://some-random-api.ml/facts/dog','https://some-random-api.ml/facts/fox','https://some-random-api.ml/facts/panda']
+
+
+   var opurl= manyurl[Math.floor(Math.random() * manyurl.length)];
+   console.log(opurl)
+   const response = await fetch(opurl);
+   let  data =await response.json();
+   var quoteText =data["fact"]
+  // var authorName =data[0]["author"]
+   console.log(quoteText)
+   document.getElementById("op").innerHTML = quoteText;
+})();
